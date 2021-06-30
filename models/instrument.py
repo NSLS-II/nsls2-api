3a7d3d7bc9f4574359962c4422ca7c3e9123db2e
Stuart Campbell <scampbell@bnl.gov>
2021-06-30 17:23:46 -0400

    Initial instrument model

---
+++ models/instrument.py
@@ -0,0 +1,12 @@
+from typing import Optional, List
+
+from pydantic.main import BaseModel
+
+
+class Instrument(BaseModel):
+    name: str
+    full_name: str
+    port_name: str
+    description: str
+    network_name: Optional[str]
+    endstations: List[str] = []
