e6c62371eac60189d888e62d4893ef3a03088780
Stuart Campbell <scampbell@bnl.gov>
2021-06-30 16:53:40 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import fastapi
+import uvicorn
+from starlette.staticfiles import StaticFiles
+
+app = fastapi.FastAPI()
+
+def configure_routing():
+    app.mount('/static', StaticFiles(directory='static'), name='static')
+    app.include_router(home.router)
+
+def configure():
+    configure_routing()
+
+if __name__ == '__main__':
+    configure()
+    uvicorn.run()
+else:
+    configure()
