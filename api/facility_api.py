d2ddfcf628b70f1ef203e3c22d6710ca59cfd1f8
Stuart Campbell <scampbell@bnl.gov>
2021-08-01 22:49:10 -0400

    Added some facility details

--- api/facility_api.py
+++ api/facility_api.py
@@ -3,23 +3,26 @@ from typing import List
 import fastapi
 
 from services.pass_service import get_cycles_async
-from models.facility import Facility
+from models.facility import Facility, FacilityName
 
 router = fastapi.APIRouter()
 
-
+facility_data = {
+    'nsls2': {'name': 'NSLS-II', 'fullname': 'National Synchrotron Light Source II', 'pass_facility_id': 'NSLS-II'},
+    'lbms': {'name': 'LBMS', 'fullname': '', 'pass_facility_id': 'LBMS'}
+}
 
 
 @router.get('/facility/{facility}/cycles')
-async def get_facility_cycles(facility: str):
+async def get_facility_cycles(facility: FacilityName):
     cycles = await get_cycles_async()
     return cycles
 
 
 @router.get('/facility/{facility}')
-def get_facility():
-    pass
+def get_facility(facility_id: FacilityName):
+    return facility_data[facility_id.name]
 
-@router.get('/facilities')
+@router.get('/facilities', response_model=Facility)
 def get_all_facilities():
     pass
--- api/users_api.py
+++ api/users_api.py
@@ -3,12 +3,15 @@ from fastapi import Depends
 
 from models.users import User
 from services import bnlpeople_service
+from services import pass_service
 
 router = fastapi.APIRouter()
 
 @router.get('/users/me')
 def get_current_user():
+    # TODO: This requires auth to be implemented so we know who the current user is!
     return {"username": "currentuser"}
+
 
 @router.get('/users')
 def get_users(person: User):
@@ -16,8 +19,47 @@ def get_users(person: User):
 
 @router.get('/users/{username}')
 async def get_user_by_username(person: User = Depends()):
-    person = await bnlpeople_service.get_person_by_username_async(person.username)
+    bnl_person = await bnlpeople_service.get_person_by_username_async(person.username)
+    pass_person = await pass_service.get_user(person.username)
     return person
+
+#1
+# @router.get('/user/{username}/data-admin-rights'}
+#
+#
+# @router.get("/users/{username}/proposal/count")
+#
+# /users/{username}/proposals?beamline=bmm?CYCLE=
+# --> list of proposals for that user
+#
+# cout = None
+# peroposal =
+# [
+# {
+#     proposal_id="",
+#     cycle="",
+#     data_session="", # implicit
+#     beamline=""
+# }
+# ]
+#
+# #2
+# /proposals/{proposalid}/users
+# --> list of users for that proposal
+#
+# #3
+# @router.post("/users/{username}/proposals")
+# input: list of data_sessions : str
+# return: list of ones I can see (from the input list)
+#
+#
+#
+#
+
+
+
+
+
 
 
 
