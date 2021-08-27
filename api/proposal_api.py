1a176fe86475d05255416b86453518774accc80f
Stuart Campbell <scampbell@bnl.gov>
2021-08-27 17:14:00 -0400

    Add basic endpoint for PASS to hit
    When PASS knows a proposal is updated, it will hit this endpoint

--- api/proposal_api.py
+++ api/proposal_api.py
@@ -4,7 +4,8 @@ import fastapi
 import httpx
 from fastapi import Depends, Header
 
-from models.proposal import ProposalIn
+from models.proposal import ProposalIn, ProposalUpdate
+from api.facility_api import facility_data
 
 from services import pass_service
 
@@ -21,13 +22,22 @@ async def get_proposal(proposal_id: ProposalIn = Depends()):
     proposal = await pass_service.get_proposal(proposal_id.proposal_id)
     return proposal
 
+
+@router.put('/proposal/{proposal_id}')
+async def update_proposal(proposal: ProposalUpdate = Depends()):
+    facility_info = facility_data[proposal.facility.name]
+    msg = {
+        'message': f'I am new going to update my local information for Proposal ID {proposal.proposal_id} for the '
+                   f'{facility_info["pass_facility_id"]} facility.'}
+    return msg
+
 @router.get('/saf/{proposal_id}')
 async def get_proposal(proposal_id: ProposalIn = Depends()):
     saf = await pass_service.get_saf_from_proposal(proposal_id.proposal_id)
     return saf
 
-#URL = "http://n2snadmin.nsls2.bnl.gov:5000"
-#client_to_windows = httpx.AsyncClient(base_url=URL, headers={"X-API-KEY": n2sn_service.api_key})
+# URL = "http://n2snadmin.nsls2.bnl.gov:5000"
+# client_to_windows = httpx.AsyncClient(base_url=URL, headers={"X-API-KEY": n2sn_service.api_key})
 
 
 # X-API-KEY
--- models/proposal.py
+++ models/proposal.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 
 from pydantic.main import BaseModel
+from .facility import FacilityName
 
 
 class ProposalIn(BaseModel):
@@ -12,3 +13,8 @@ class ProposalIn(BaseModel):
                 "proposal_id": 304947,
             }
         }
+
+
+class ProposalUpdate(BaseModel):
+    proposal_id: int
+    facility: FacilityName = FacilityName.nsls2
